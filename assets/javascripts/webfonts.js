6158935e3bf04982cd6922c8b67c8a8bff4a8b58
Kaitlin Seng <kaitmseng@gmail.com>
2025-05-11 04:43:33 -0400

    Tweak style and fix broken references

--- assets/javascripts/webfonts.js
+++ assets/javascripts/webfonts.js
@@ -1,7 +1,8 @@
 //= require webfontloader/webfontloader.js
 
-WebFont.load({
-  google: {
-    families: ['Cormorant Garamond:700', 'Lato:300,400,700']
-  }
-});
+(function(d) {
+  var wf = d.createElement('script'), s = d.scripts[0];
+  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
+  wf.async = true;
+  s.parentNode.insertBefore(wf, s);
+})(document);
